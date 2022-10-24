633fb3d08bc7113eba7fa17a931181910c489c53
funkyman3121 <alvinmasaba@gmail.com>
2022-10-24 12:15:26 -0400

    Adds basic search form to flights index with select dropdowns. Defines #flight_date method in Flight model to retrieve verbose flight date.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,8 @@ class Flight < ApplicationRecord
 
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def flight_date
+    self.date.strftime("%B %-d, %Y")
+  end
 end
